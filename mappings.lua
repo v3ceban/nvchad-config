3f560622dadbeb6af357af35ba4f3047651c1c9f
Vladimir Ceban <v3ceban@proton.me>
2023-10-11 13:04:58 -0700

    mappings updagte

--- mappings.lua
+++ mappings.lua
@@ -1,10 +1,15 @@
 local M = {}
 
 M.general = {
+  i = {
+    ["<C-s>"] = { "<cmd> wa <CR>", "Save all files" },
+  },
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["'"] = { ":", "enter command mode", opts = { nowait = true } },
     ['"'] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<C-s>"] = { "<cmd> wa <CR>", "Save all files" },
+    ["<C-q>"] = { "<cmd> qa <CR>", "Quit everything" },
   },
 }
 
