db23d4f0f090c6957a366691c49607dbdcae6cad
Vladimir Ceban <v3ceban@proton.me>
2023-10-20 20:30:22 -0700

    mappings update

--- chadrc.lua
+++ chadrc.lua
@@ -6,6 +6,18 @@ M.ui = {
   statusline = {
     separator_style = "arrow",
   },
+  nvdash = {
+    load_on_startup = true,
+    buttons = {
+      { "󰙅  Open Project Tree", "<C-n>", "NvimTreeFocus" },
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
 }
 
 M.plugins = "custom.plugins"
--- mappings.lua
+++ mappings.lua
@@ -5,14 +5,34 @@ M.general = {
     ["<C-s>"] = { "<cmd> wa <CR>", "Save all files" },
   },
   n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["'"] = { ":", "enter command mode", opts = { nowait = true } },
-    ['"'] = { ":", "enter command mode", opts = { nowait = true } },
+    [";"] = { ":", "Enter command mode", opts = { nowait = true } },
+    ['"'] = { "<nop>" },
     ["<C-s>"] = { "<cmd> wa <CR>", "Save all files" },
     ["<C-q>"] = { "<cmd> qa <CR>", "Quit everything" },
+    ["<leader>s"] = { [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gc<Left><Left><Left>]], "Search and replace" },
   },
   v = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    [";"] = { ":", "Enter command mode", opts = { nowait = true } },
+    ["J"] = { ":m '>+1<CR>gv=gv", "Move selection down" },
+    ["K"] = { ":m '<-2<CR>gv=gv", "Move selection up" },
+  },
+}
+
+M.chatgpt = {
+  n = {
+    ["<leader>Cc"] = { "<cmd>ChatGPT<CR>", "ChatGPT" },
+    ["<leader>Ce"] = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction" },
+    ["<leader>Cg"] = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction" },
+    ["<leader>Ct"] = { "<cmd>ChatGPTRun translate<CR>", "Translate" },
+    ["<leader>Ck"] = { "<cmd>ChatGPTRun keywords<CR>", "Keywords" },
+    ["<leader>Cd"] = { "<cmd>ChatGPTRun docstring<CR>", "Docstring" },
+    ["<leader>Ca"] = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests" },
+    ["<leader>Co"] = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code" },
+    ["<leader>Cs"] = { "<cmd>ChatGPTRun summarize<CR>", "Summarize" },
+    ["<leader>Cf"] = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs" },
+    ["<leader>Cx"] = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code" },
+    ["<leader>Cr"] = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit" },
+    ["<leader>Cl"] = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis" },
   },
 }
 
