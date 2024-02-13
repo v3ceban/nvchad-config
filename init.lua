1e185e6fcbce4bf6e5d5c729084bcca07b56c8f7
Super User <root@localhost.localdomain>
2024-02-13 14:47:53 -0800

    set default shell to zsh

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 vim.opt.relativenumber = true --relative numbers
 vim.opt.wrap = false --don't wrap long lines
 vim.opt.incsearch = true --allows for tricky searches like "v* = false"
+vim.opt.shell = "/bin/zsh" --set shell to zsh
