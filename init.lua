d580a5b74aa50cf9201eead8c2663fe6ef013c50
Vladimir Ceban <v3ceban@proton.me>
2024-02-21 10:49:43 -0800

    folding

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
-vim.opt.relativenumber = true --relative numbers
-vim.opt.wrap = false --don't wrap long lines
-vim.opt.incsearch = true --allows for tricky searches like "v* = false"
-vim.opt.shell = "/bin/zsh" --set shell to zsh
+vim.opt.relativenumber = true                   --relative numbers
+vim.opt.wrap = false                            --don't wrap long lines
+vim.opt.incsearch = true                        --allows for tricky searches like "v* = false"
+vim.opt.shell = "/bin/zsh"                      --set shell to zsh
+vim.opt.foldmethod = "expr"                     --set foldmethod to expr
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()" --set foldexpr to nvim_treesitter#foldexpr()
