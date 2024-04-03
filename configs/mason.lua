5993f3957873dcfb9ae523607cf49a4d9c4f6bbd
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2024-04-02 23:41:52 -0700

    ruby lsp added to mason req

--- configs/mason.lua
+++ configs/mason.lua
@@ -34,6 +34,9 @@ local mason = {
     "goimports-reviser", --go formater for imports ($go install -v github.com/incu6us/goimports-reviser/v3@latest)
     "golines",           --go formatter for line length
 
+    --ruby
+    "solargraph", --ruby lsp and linter
+
     --bash
     "bash-language-server",
   },
