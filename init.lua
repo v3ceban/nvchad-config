4ebe1412fe26772ad217cbae7a888548ec99a9bf
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2024-08-23 12:45:43 -0700

    removed wrapping

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 vim.opt.relativenumber = true                   --relative numbers
-vim.opt.wrap = true                             --don't wrap long lines
-vim.opt.linebreak = true                        --wrap at word boundaries
+vim.opt.wrap = false                            --don't wrap long lines
 vim.opt.incsearch = true                        --allows for tricky searches like "v* = false"
 vim.opt.shell = "/bin/zsh"                      --set shell to zsh
 vim.opt.foldmethod = "expr"                     --set foldmethod to expr
