72b67afcf991c6539a5a161fa731718784827607
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2024-08-23 00:32:58 -0700

    linebreaks

--- init.lua
+++ init.lua
@@ -1,17 +1,9 @@
 vim.opt.relativenumber = true                   --relative numbers
-vim.opt.wrap = false                            --don't wrap long lines
+vim.opt.wrap = true                             --don't wrap long lines
+vim.opt.linebreak = true                        --wrap at word boundaries
 vim.opt.incsearch = true                        --allows for tricky searches like "v* = false"
 vim.opt.shell = "/bin/zsh"                      --set shell to zsh
 vim.opt.foldmethod = "expr"                     --set foldmethod to expr
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()" --set foldexpr to nvim_treesitter#foldexpr()
 vim.opt.foldlevel = 99                          --set foldlevel to 99
 vim.g.copilot_no_tab_map = true                 --disable tab mapping for copilot
-
--- enable wrap for json and markdown files
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "json", "markdown" },
-  callback = function()
-    vim.opt_local.wrap = true
-    vim.opt_local.linebreak = true
-  end,
-})
