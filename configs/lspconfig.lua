bc81cbc364442c710a2804dc00a462d0e3fa943b
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2024-09-12 00:57:12 -0700

    lsp config upd

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -33,8 +33,8 @@ lspconfig.clangd.setup {
   on_attach = on_attach,
 }
 
--- customized setup for tsserver
-lspconfig.tsserver.setup {
+-- customized setup for ts_ls
+lspconfig.ts_ls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   init_options = {
