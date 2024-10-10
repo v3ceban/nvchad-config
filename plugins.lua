084169b3c9e253e52b8d948b053c2a3ad11b2eed
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2024-10-09 23:27:09 -0700

    added troubple to chatgpt.nvim deps

--- plugins.lua
+++ plugins.lua
@@ -75,6 +75,7 @@ local plugins = {
     dependencies = {
       "MunifTanjim/nui.nvim",
       "nvim-lua/plenary.nvim",
+      "folke/trouble.nvim",
       "nvim-telescope/telescope.nvim",
     },
     config = function()
