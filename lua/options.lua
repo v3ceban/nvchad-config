0865a191e227bb65d282e0d198b7277e17b609a7
Vladimir Ceban <v3ceban@proton.me>
2025-03-13 16:25:24 -0700

    feat(quickfix): enhance quickfix window navigation
    Add additional key mappings for the quickfix window:
    - 'o' to open selected item without closing the quickfix window
    - 'Esc' to close the quickfix window
    
    These improvements make quickfix window navigation more intuitive and
    aligned with common UI patterns.

--- lua/options.lua
+++ lua/options.lua
@@ -48,5 +48,7 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "qf",
   callback = function()
     vim.api.nvim_buf_set_keymap(0, "n", "<CR>", "<CR>:cclose<CR>", { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, "n", "o", "<CR>", { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, "n", "<Esc>", ":cclose<CR>", { noremap = true, silent = true })
   end,
 })
