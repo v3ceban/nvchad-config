b262cfb3931067c9fd12638c820d0a2c7e2f19d5
Vladimir Ceban <v3ceban@proton.me>
2025-04-08 13:00:15 -0700

    chore(conform): increase format timeout to 2000ms
    Increases the timeout for format_on_save and default_format_opts from 1000ms
    to 2000ms to allow more time for larger files to be formatted properly.

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -60,12 +60,12 @@ local options = {
   },
 
   format_on_save = {
-    timeout_ms = 1000,
+    timeout_ms = 2000,
     lsp_format = "fallback",
   },
 
   default_format_opts = {
-    timeout_ms = 1000,
+    timeout_ms = 2000,
     lsp_format = "fallback",
   },
 }
